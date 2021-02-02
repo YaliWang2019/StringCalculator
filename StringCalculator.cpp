12a2ec4f2a4ee1a9d54b1e6a4239393ea52250fb
id <id@kl>
2021-02-01 23:08:41 -0900

    Implement specification of single char delimiter
    Generalized delimiter parsing to use an unordered
    set of chars; if first char is non-numeric, and not
    a '-' sign, stores in delimiter char set.
    
    Added simple function to check containment of char
    within unordered set; looks like ::contains(T) will
    be added to unordered set in future C++ standard.

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,7 +2,14 @@
 #include "StringCalculator.hpp"
 #include <stdexcept>
 #include <string>
+#include <unordered_set>
 #include <vector>
+
+bool CharSetContains(std::unordered_set<char> char_set, const char c)
+{
+	auto set_check = char_set.find(c);
+	return set_check != char_set.end();
+}
 
 StringCalculator::StringCalculator() {};
 
@@ -13,18 +20,28 @@ int StringCalculator::Add(std::string numbers)
 	if (!numbers.empty())
 	{
 		// the general idea is to create a vector holding each number
-		// from the input as distinct strings, which summed up at the
+		// from the input as distinct strings, which is summed up at the
 		// end -- the loop runs through the input string until a
 		// delimiting character is seen, then begins the next entry
 
 		std::vector<std::string> entries;
+		std::unordered_set<char> delims;
+		delims.insert('\n');
+		delims.insert(',');
+
+		if (!isdigit(numbers[0]) && numbers[0] != '-')
+		{
+			delims.insert(numbers[0]);
+			numbers = numbers.substr(1);
+		}
 
 		entries.push_back(numbers.substr(0, 1));
 		int current_entry = 0;
 
 		for (int i = 1; i < numbers.length(); i++)
 		{
-			if (numbers.substr(i, 1) == "\n" || numbers.substr(i, 1) == ",") {
+			//if (numbers.substr(i, 1) == "\n" || numbers.substr(i, 1) == ",") {
+			if (CharSetContains(delims,numbers[i])) {
 				current_entry++;
 				i++;
 				entries.push_back(numbers.substr(i, 1));
